748e1d4a01ef331e98e80746d4d1110fc02a0d36
Michael Menzies <michael@dwarfstar.co.nz>
2018-02-26 15:02:46 +1300

    Added mongoose. Section 3 - Lecture 23

--- routes/app.js
+++ routes/app.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
 });
 
-router.get('/message/:msg', function (req, res, next) {
-
-    res.render('node', {message: req.params.msg});
+router.post('/', function (req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'New',
+        lastName: 'User',
+        password: 'password',
+        email: email
+    });
+    console.log('adding email ' + email);
+    user.save(function callback(err, res)  {console.error(err);});
 });
-
-router.post('/message', function (req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/'+ message);
-});
-
 
 module.exports = router;
